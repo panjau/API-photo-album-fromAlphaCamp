95b7e145b05672f6b76baab7a7b1d4dc12416629
allenpan <panjau@gmail.com>
2018-06-04 17:06:08 +0800

    grab from alphacamp, remove git log, and first time commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "photos#index"
+  resources :photos
+  resources :users
+
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+
+      resources :photos, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
+end
